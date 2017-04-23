a29bed91dc92bb5cc6b3ae0c6f021b0fc8a43c31
Kyle Sammons <ksammon2@uoregon.edu>
2017-04-23 11:04:42 -0700

    Added basic gravity wells

---
+++ Assets/Scripts/Managers/GravityWellManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityWellManager : MonoBehaviour
+{
+    public Vector2 gravity;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "Player")
+        {
+            Physics2D.gravity = gravity;
+        }
+    }
+}
+
