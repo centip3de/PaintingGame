760e6569d15b5838d7f48804be8f64d4d5dd2c24
Forest Belton <forest@homolo.gy>
2017-04-23 16:54:37 -0700

    u

---
+++ Assets/Scripts/Input/KeyDownEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace AssemblyCSharp {
+
+	public class KeyDownEvent : EventArgs {
+
+		public KeyDownEvent (KeyCode keyCode) {
+			code = keyCode;
+		}
+
+		private KeyCode code;
+
+		public KeyCode keyCode {
+			get {
+				return code;
+			}
+		}
+	}
+}
